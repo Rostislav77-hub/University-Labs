38bd78c8685f6f98e0d8a0c30fb2b862b99603af
Rostislav <rostikmatv123@gmail.com>
2025-11-24 22:03:54 +0200

    remove.js

---
+++ Laboratorna7.js/remove.js
@@ -0,0 +1,17 @@
+function removeElement(array, item) {
+  const index = array.indexOf(item);
+  if (index !== -1) {
+    array.splice(index, 1);
+  }
+}
+
+console.log('=== Задание 1: removeElement ===');
+
+const array1 = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array1, 5);
+console.log('Удалили 5:', array1);
+
+const array2 = ['Kiev', 'Beijing', 'Lima', 'Saratov'];
+removeElement(array2, 'Lima');
+removeElement(array2, 'Berlin');
+console.log('Удалили Lima и Berlin:', array2);
